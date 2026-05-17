861b51d48a98b0914a00f90ffe8582e6ce1c92eb
madnessmichael <bimnaxu753@gmail.com>
2026-05-17 12:22:42 +0000

    bigshot on A40

--- scripts/agopnullspace_steer.sh
+++ scripts/agopnullspace_steer.sh
@@ -33,6 +33,17 @@ python ./src/calc_steering_matrix_rfm.py \
     --layers 28,30,32,34,36,38,40,42,44,46,48,50
 
 
+
+# Steering!
+GENERATE_CONFIG_DIR=config/llama3.3-70b_rfm
+echo "Generating response for $NICKNAME"
+for file in $GENERATE_CONFIG_DIR/*.yaml; do
+    filename=$(basename "$file" .yaml)
+    echo "Generating response for $file"
+    python src/generate_response.py --config_path $file
+done
+
+
 python ./src/calc_steering_matrix_rfm.py \
     --model_name llama3.1 \
     --embedding_dir data/embeddings/llama3.1 \
@@ -61,6 +72,17 @@ python src/calc_steering_matrix_rfm_no_nullspace.py \
     --save_path     data/steering_matrix/steering_matrix_llama3.3-70b_rfm_no_nullspace.pt \
     --rfm_method    rfm \
     --layers        28,30,32,34,36,38,40,42,44,46,48,50
+
+
+# Steering!
+GENERATE_CONFIG_DIR=config/llama3.3-70b_rfm_no_nullspace
+echo "Generating response for $NICKNAME"
+for file in $GENERATE_CONFIG_DIR/*.yaml; do
+    filename=$(basename "$file" .yaml)
+    echo "Generating response for $file"
+    python src/generate_response.py --config_path $file
+done
+
 
 # ── llama3.1 ─────────────────────────────────────────────────────────────────
 python src/calc_steering_matrix_rfm_no_nullspace.py \
